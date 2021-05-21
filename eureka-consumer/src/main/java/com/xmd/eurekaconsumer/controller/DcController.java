c47b0d780d0593cac2221095e5ec9bb44c0ffc51
xumengdi <15189835655@163.com>
2021-05-21 17:12:18 +0800

    添加swagger配置2021年5月21日17:12:14

--- eureka-consumer/src/main/java/com/xmd/eurekaconsumer/controller/DcController.java
+++ eureka-consumer/src/main/java/com/xmd/eurekaconsumer/controller/DcController.java
@@ -1,13 +1,23 @@
 package com.xmd.eurekaconsumer.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+
+@Api(tags = "消费者服务")
 @RestController
+@Validated
+@Slf4j
+@RequestMapping("/api")
 public class DcController {
     @Autowired
     LoadBalancerClient loadBalancerClient;
@@ -15,6 +25,7 @@ public class DcController {
     RestTemplate restTemplate;
 
     @GetMapping("/consumer")
+    @ApiOperation("获取服务列表")
     public String dc() {
         ServiceInstance serviceInstance = loadBalancerClient.choose("eureka-client");
         String url = "http://" + serviceInstance.getHost() + ":" + serviceInstance.getPort() + "/dc";
