8cf0ecc2ced6c2401535de819dfa67cbea7d283e
xumengdi <15189835655@163.com>
2021-05-21 14:16:33 +0800

    添加启动日志2021年5月21日14:16:30

--- common/src/main/java/com/xmd/common/CommonApplication.java
+++ common/src/main/java/com/xmd/common/CommonApplication.java
@@ -1,13 +1,42 @@
 package com.xmd.common;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @SpringBootApplication
 public class CommonApplication {
 
+	private static Logger logger = LoggerFactory.getLogger(CommonApplication.class);
+
 	public static void main(String[] args) {
-		SpringApplication.run(CommonApplication.class, args);
+//		SpringApplication.run(CommonApplication.class, args);
+
+		SpringApplication app = new SpringApplication(CommonApplication.class);
+		Environment env = app.run(args).getEnvironment();
+		String protocol = "http";
+		try {
+			logger.info("\n----------------------------------------------------------\n\t" +
+							"Application '{}' is running! Access URLs:\n\t" +
+							"Local: \t\t{}://localhost:{}\n\t" +
+							"External: \t{}://{}:{}\n\t" +
+							"swagger: \t{}://localhost:{}/swagger-ui.html\n----------------------------------------------------------",
+					env.getProperty("spring.application.name"),
+					protocol,
+					env.getProperty("server.port"),
+					protocol,
+					InetAddress.getLocalHost().getHostAddress(),
+					env.getProperty("server.port"),
+					protocol,
+					env.getProperty("server.port"));
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
