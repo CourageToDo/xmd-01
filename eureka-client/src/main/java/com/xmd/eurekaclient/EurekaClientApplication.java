1ef1064e088372646f949bfcf297567575ed3469
xumengdi <15189835655@163.com>
2021-05-21 17:00:33 +0800

    整合swagger2021年5月21日17:00:27

--- eureka-client/src/main/java/com/xmd/eurekaclient/EurekaClientApplication.java
+++ eureka-client/src/main/java/com/xmd/eurekaclient/EurekaClientApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
 import java.net.InetAddress;
@@ -13,6 +14,7 @@ import java.net.UnknownHostException;
 
 @EnableDiscoveryClient
 @SpringBootApplication
+//@ComponentScan(basePackages = {"com.xmd.eurekaclient.config"})
 public class EurekaClientApplication {
 
     private static Logger logger = LoggerFactory.getLogger(EurekaClientApplication.class);
